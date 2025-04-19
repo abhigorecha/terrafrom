e52afb0a1c3cd71fd2022c17a623b6d6b497cfac
Abhishek Gorecha <abhishekgorecha9@gmail.com>
2025-04-19 14:37:14 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "test_ec2" {
-  ami           = "ami-002f6e91abff6eb96"
-  instance_type = "t2.micro"
+  ami           = var.ami
+  instance_type = var.instance
   key_name     = "pranav"
 
   tags = {
